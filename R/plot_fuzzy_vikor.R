3522a415971ee447ba17e41f78bf95bbf8ba5904
anbuszek <an.buszek@student.uj.edu.pl>
2025-12-17 11:49:34 +0100

    3

--- R/plot_fuzzy_vikor.R
+++ R/plot_fuzzy_vikor.R
@@ -5,14 +5,13 @@
 #' @export
 plot.fuzzy_vikor_res <- function(x, ...) {
   df <- x$results
-  stopifnot(all(c("Def_S", "Def_R", "Def_Q") %in% names(df)))
 
   plot(
-    df$Def_S, df$Def_R,
-    cex = sqrt(pmax(df$Def_Q, 0)) + 0.5,
+    df$TotalLoss, df$MaxLoss,
+    cex = sqrt(pmax(df$Compromise, 0)) + 0.5,
     pch = 19,
-    xlab = "S (defuzzified)",
-    ylab = "R (defuzzified)",
+    xlab = "TotalLoss",
+    ylab = "MaxLoss",
     ...
   )
 
